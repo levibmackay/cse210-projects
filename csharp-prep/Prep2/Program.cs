b434cb889830ef31dd44e36a288b8461c5ef8b14
Levi Mackay <mac21048@byui.edu>
2026-01-10 10:02:58 -0700

    W1 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter Percentage: ");
+        string response = Console.ReadLine();
+        int percent = int.Parse(response);
+
+        string letterGrade = "";
+        string sign = "";
+
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        int lastDigit = percent % 10;
+
+        if (letterGrade != "A" && letterGrade != "F")
+        {
+            if (lastDigit >= 7)
+                sign = "+";
+            else if (lastDigit <= 2)
+                sign = "-";
+        }
+
+        if (letterGrade == "A" && percent < 93)
+            sign = "-";
+
+        Console.WriteLine($"Your grade: {letterGrade}{sign}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. You'll get them next time.");
+        }
     }
 }
